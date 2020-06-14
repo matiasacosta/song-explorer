0bf6675a355056f95330ae4e8b0397511d12ebe6
Matias Acosta <matiasacosta1995@gmail.com>
2020-06-14 01:51:18 -0300

    Add dark theme and app function cmpt

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import { SearchBox } from '../SearchBox/SearchBox';
-import { Player, Track } from '../Player/Player';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { SearchBox } from "../SearchBox/SearchBox";
+import { Player, Track } from "../Player/Player";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline, Paper, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+/* Styles */
+const customTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#11cb5f",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  title: {
+    margin: theme.spacing(2)
+  },
+  loginButton: {
+    margin: theme.spacing(2)
+  }
+}));
+/* End Styles */
+
+const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "";
 const redirectUri = "http://localhost:3000";
 
-interface IState{
+interface IState {
   token: string;
   songName: string;
-  track: Track;
+  track?: Track;
 }
 
 // Get the hash of the url
 let hash = window.location.hash
   .substring(1)
   .split("&")
-  .map(function(item){
+  .map(function (item) {
     if (item) {
       let parts: string[] = item.split("=");
-      if(parts[0] === "access_token"){
+      if (parts[0] === "access_token") {
         return parts[1];
       }
     }
@@ -30,77 +60,85 @@ let hash = window.location.hash
 
 window.location.hash = "";
 
-class App extends React.Component<{}, IState> {
-  
-  public constructor(props: {}){
-    super(props);
-    this.state = {
-      token: "",
-      songName: "",
-      track: {name:"",artists:[],image:"",preview:""}
-    }
-  }
+export const App: React.FC = () => {
+  const classes = useStyles();
 
-  public componentDidMount() {
-    for (let value of hash){
+  const [token, setToken] = useState("");
+  const [songName, setSongName] = useState("");
+  const [track, setTrack] = useState<Track>();
+
+  useEffect(() => {
+    for (let value of hash) {
       if (value !== "") {
         // Set token
-        this.setState({token:value});
+        setToken(value);
       }
-    }  
-  }
+    }
+  }, [token]);
 
-  private handleSongNameChange = (songName: string) => {
-    this.setState({ songName });
-  }
+  const handleSongNameChange = (songName: string) => {
+    setSongName(songName);
+  };
 
-  private handleClick = () => {
-    const headers = new Headers({'Authorization': `Bearer ${this.state.token}`});
-    const init = {method: "GET", headers: headers};
+  const handleClick = () => {
+    const headers = new Headers({ Authorization: `Bearer ${token}` });
+    const init = { method: "GET", headers: headers };
 
-    if(this.state.token !== ""){
-      let songName = this.state.songName.replace(" ","%20");
-      fetch(`https://api.spotify.com/v1/search?q=${songName}&type=track&limit=1`, init)
-      .then(response => response.json())
-      .then(response => {
+    if (token !== "") {
+      let songNameUrl = songName.replace(" ", "%20");
+      fetch(
+        `https://api.spotify.com/v1/search?q=${songNameUrl}&type=track&limit=1`,
+        init
+      )
+        .then((response) => response.json())
+        .then((response) => {
           let trackItem = response.tracks.items[0];
           let newTrack: Track = {
             name: trackItem.name,
-            artists: trackItem.artists.map(
-              (artist:any) => { return {name:artist.name}}
-            ),
+            artists: trackItem.artists.map((artist: any) => {
+              return { name: artist.name };
+            }),
             image: trackItem.album.images[1].url,
-            preview: trackItem.preview_url
-          }
-          this.setState({track:newTrack});
-      })
-      .catch(error => console.log("Error"));
+            preview: trackItem.preview_url,
+          };
+          setTrack(newTrack);
+        })
+        .catch((error) => console.log("Error"));
     }
-  }
-
-  public render(){
-    return (
-      <div className="App">
-        {!this.state.token && (
-          <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}
-          >
-          Login to Spotify
-          </a>
-        )}
-        {this.state.token && (
-          <React.Fragment>
-            <h1>Song Explorer</h1>
-            <SearchBox songName={this.state.songName} onSongNameChange={this.handleSongNameChange} onClick={this.handleClick}/>
-          </React.Fragment>
-        )}
-        {this.state.track && this.state.token &&(
-          <Player track={this.state.track}/>
-        )}
-      </div>
-    )
   };
-}
+
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Container maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+          <Typography variant="h3" className={classes.title} gutterBottom>
+            Song Explorer
+          </Typography>
+          {!token && (
+            <Button
+              className={classes.loginButton}
+              variant="outlined"
+              color="primary"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}
+            >
+              Login With Spotify
+            </Button>
+          )}
+          {token && (
+            <React.Fragment>
+              <SearchBox
+                songName={songName}
+                onSongNameChange={handleSongNameChange}
+                onClick={handleClick}
+              />
+            </React.Fragment>
+          )}
+          {track && token && <Player track={track} />}
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
